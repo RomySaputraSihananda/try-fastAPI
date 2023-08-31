73696536a7c983189e038a644b6a339f6c3a7566
romys <romys.1222@gmail.com>
2023-08-31 20:12:12 +0700

    add main program

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ async def root():
 
 @app.get('/product/{category}/')
 async def read_item(page: int = 0, category: str = ''):
+    
     data = get_data('Elektronik', page);
     respone = gen_respone(data);
+    
     return respone;

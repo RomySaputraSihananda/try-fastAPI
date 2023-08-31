7de1dbf09025e3431e3d5525be24a4a81b313d72
romys <romys.1222@gmail.com>
2023-08-31 20:12:03 +0700

    add main program

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI;
+from utils import get_data, gen_respone;
+
+app = FastAPI();
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"};
+
+
+@app.get('/product/{category}/')
+async def read_item(page: int = 0, category: str = ''):
+    data = get_data('Elektronik', page);
+    respone = gen_respone(data);
+    return respone;
